b85cf069d9f929b295a66e37837f245a37a12ef2
Shubhasis Mathur <shmat@microsoft.com>
2023-08-10 15:31:32 +0530

    Added a new API ProductDetails

---
+++ src/Services/ProductInfo/Controllers/ProductDetails.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductInfo.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductDetailsController : Controller
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Content("This is a dummy string.");
+        }
+
+    }
+}
---
+++ src/Services/ProductInfo/Program.cs
@@ -0,0 +1,17 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
